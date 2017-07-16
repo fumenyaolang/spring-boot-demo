e97beb1b959af2cc360f63f291759b4c87221e33
fumenyaolang <fuzq1919@hotmail.com>
2017-07-17 00:23:46 +0800

    v0

--- src/test/java/com/fmyl/demo/test/controller/UserControllerTest.java
+++ src/test/java/com/fmyl/demo/test/controller/UserControllerTest.java
@@ -1,16 +1,42 @@
 package com.fmyl.demo.test.controller;
 
 import com.fmyl.demo.controller.UserController;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Created by fuzq on 17/7/7.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = UserController.class)
-//@WebIntegrationTest
+@SpringBootTest(classes = UserController.class)
+@WebAppConfiguration
 public class UserControllerTest {
+
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() {
+        mvc = MockMvcBuilders.standaloneSetup(new UserController()).build();
+    }
+
+    @Test
+    public void testVeiw() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/user/1")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("{\"id\":1,\"name\":\"zhang\",\"age\":null}")));
+    }
 
 }
--- src/test/java/com/fmyl/demo/test/service/UserServiceTest.java
+++ src/test/java/com/fmyl/demo/test/service/UserServiceTest.java
@@ -1,16 +1,11 @@
 package com.fmyl.demo.test.service;
 
-import com.fmyl.demo.service.UserService;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 /**
  * user service test
  * Created by fuzq on 17/7/7.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = UserService.class)
-public class UserServiceTest {
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringBootTest
+class UserServiceTest {
 
 }
